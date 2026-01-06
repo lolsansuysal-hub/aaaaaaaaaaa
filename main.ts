701e68ddaecbb37f4ab448eb7d2b78e5ec7da924
lolsansuysal-hub <lolsansuysal@gmail.com>
2026-01-06 17:38:11 +0300

    aaa

--- main.ts
+++ main.ts
@@ -13,5 +13,5 @@ input.onButtonPressed(Button.B, function () {
     music.stopMelody(MelodyStopOptions.All)
 })
 basic.forever(function () {
-    music.play(music.stringPlayable("C5 E D C5 A E E F ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.UntilDone)
 })
